d0b3eaf99dd6b7dbee1d62a54f5e6b7e01948af6
Ethan Buitrago <ethanbuicas@gmail.com>
2021-08-03 20:15:48 -0500

    GitHub actions

---
+++ operaciones.js
@@ -0,0 +1,14 @@
+exports.addTest= function suma(num1,num2){
+    return num1+num2
+}
+
+exports.resTest = function resta(num1,num2){
+    return num1-num2
+}
+
+exports.multTest=function mult(num1,num2){
+    return num1*num2
+}
+exports.diviTest = function div(num1,num2){
+    return num1/num2
+}
---
+++ test/operaciones-test.js
@@ -0,0 +1,51 @@
+var chai = require('chai');
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+var calculator = require('../operaciones');
+
+describe('Testing de la manera assert : ', function() {
+  describe('funciones de suma y resta', function(){
+    it('Suma', function() {
+       result = calculator.addTest(1,1);
+       assert.equal(result, 2);
+
+       
+    });
+
+    it('resta', function() {
+    result = calculator.resTest (1,1);
+       assert.equal(result, 0);
+    });
+  });
+})
+
+
+describe('Testing de la manera should: ', function() {
+    describe('funciones de suma y resta', function(){
+      it('Suma', function() {
+         result = calculator.addTest(1,1);
+         result.should.be.equal(2);
+      })
+
+      it('resta', function() {
+        result = calculator.resTest (1,1);
+        result.should.be.equal(0);
+          
+        });
+    })
+  })
+
+  describe('TTesting de la manera expect: ', function() {
+    describe('funciones de suma y resta', function(){
+      it('Suma', function() {
+         result = calculator.addTest(1,1);
+          expect(result).to.equal(2);
+      })
+      it('resta', function() {
+            result = calculator.resTest (1,1);
+            expect(result).to.equal(0);
+           
+        });
+    })
+  })
